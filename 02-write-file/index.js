debe56c27a4c3e997cde11786dfa6f29791d107b
Olena Honcharuk <gasmin07@gmail.com>
2021-11-01 22:19:40 +0200

    fix: escape 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,36 +1,32 @@
 const fs = require("fs");
 const path = require("path");
 const readline = require("readline");
-const { stdin: input, stdout: output } = require("process");
-const rl = readline.createInterface({ input, output });
+const rl = readline.createInterface(process.stdin, process.stdout);
 const stdout = process.stdout;
 const stdin = process.stdin;
 const filePath = path.join(__dirname, "answer.txt");
 
 rl.question("Hi! What do you think of Node.js?\n", (data) => {
-  stdout.write(
-    `You can writedown something or To exit, press ^C or type .exit\n`
-  );
-  stdin.on("data", (data) => {
-    answer = data.toString().trim();
-    if (answer === "exit") {
-      stdout.write(`Good luck,\n exit with code: ${code}`);
-      process.exit();
-    }
+  rl.on("line", function (line) {
+    if (line === "exit") rl.close();
+  }).on("close", function () {
+    process.exit(0);
   });
-  fs.writeFile(filePath, data, (err) => {
+  stdout.write(
+    `You can writedown something or To exit, press ^C or type exit\n`
+  );
+  fs.writeFile(filePath, data + "\n", (err) => {
     if (err) throw err;
   });
+
   stdin.on("data", (data) => {
     fs.appendFile(filePath, data, (err) => {
       if (err) throw err;
     });
-
-    //
   });
 });
 process.on("exit", (code) => {
   rl.close();
   stdout.write(`Good luck,\nexit with code: ${code}`);
-  process.exit(0);
+  process.exit();
 });
