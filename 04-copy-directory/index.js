d95d9363a2ef38434b013188ccfd89ce3d380997
Olena Honcharuk <gasmin07@gmail.com>
2021-11-09 23:21:47 +0200

    fix: 04-copy for change folder

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,27 +3,42 @@ const path = require("path");
 let source = path.resolve(__dirname, "files");
 let destination = path.resolve(__dirname, "files-copy");
 
-fs.mkdir(destination, { recursive: true }, function (err) {
-  if (err && err.code === "EEXIST") {
-    console.log(err);
-  } else {
-    console.log("folder created");
-  }
-});
-
-fs.readdir(source, { withFileTypes: true }, (err, files) => {
-  if (err) throw err;
-  files.forEach((file) => {
-    if (file.isFile()) {
-      const filePath = path.resolve(__dirname, source, file.name);
-      const filePathNew = path.resolve(__dirname, destination, file.name);
-      fs.stat(filePath, (err, stats) => {
-        if (err) throw err;
-        fs.copyFile(filePath, filePathNew, (err) => {
-          if (err) throw err;
-          console.log(`${file.name} was copied to destination`);
-        });
-      });
+function copyFolder() {
+  fs.mkdir(destination, { recursive: true }, function (err) {
+    if (err && err.code === "EEXIST") {
+      console.log(err);
+    } else {
+      console.log("folder created");
     }
   });
+
+  fs.readdir(source, { withFileTypes: true }, (err, files) => {
+    if (err) throw err;
+
+    files.forEach((file) => {
+      if (file.isFile()) {
+        const filePath = path.resolve(__dirname, source, file.name);
+        const filePathNew = path.resolve(__dirname, destination, file.name);
+        fs.stat(filePath, (err, stats) => {
+          if (err) throw err;
+          fs.copyFile(filePath, filePathNew, (err) => {
+            if (err) throw err;
+            console.log(`${file.name} was copied to destination`);
+          });
+        });
+      }
+    });
+  });
+}
+fs.access(destination, (err) => {
+  if (err) {
+    copyFolder();
+  } else {
+    fs.rm(destination, { recursive: true }, (err) => {
+      if (err) {
+        throw err;
+      }
+      copyFolder();
+    });
+  }
 });
