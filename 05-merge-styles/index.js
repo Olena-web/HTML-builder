0c04dac5385317d8ebe6a79b36343b92625f0fde
Olena Honcharuk <gasmin07@gmail.com>
2021-11-09 23:59:11 +0200

    fix: 05-merge from ENOENT

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -3,38 +3,44 @@ const path = require("path");
 const folderPath = path.join(__dirname, "styles");
 const distPath = path.join(__dirname, "project-dist", "bundle.css");
 
-fs.readdir(folderPath, { withFileTypes: true }, (err, files) => {
+fs.open(distPath, "w", (err) => {
   if (err) throw err;
-  fs.stat(distPath, function (err, stats) {
-    console.log(stats);
-    if (err) {
-      return console.error(err);
-    }
-    fs.unlink(distPath, function (err) {
-      if (err) return console.log(err);
-      console.log("file remove for change");
+});
+function copyStyle() {
+  fs.readdir(folderPath, { withFileTypes: true }, (err, files) => {
+    if (err) throw err;
+    fs.stat(distPath, function (err, stats) {
+      console.log(stats);
+      if (err) {
+        return console.error(err);
+      }
+      // fs.unlink(distPath, function (err) {
+      //   if (err) return console.log(err);
+      //   console.log("file remove for change");
+      // });
+    });
+    files.forEach((file) => {
+      if (file.isFile()) {
+        const filePath = path.join(__dirname, "styles", file.name);
+        const fileExt = path.parse(filePath).ext;
+        fs.stat(filePath, (err, stats) => {
+          if (err) throw err;
+          if (fileExt === ".css") {
+            const stream = fs.createReadStream(filePath, "utf8");
+            let data = "";
+            stream.on("data", (partData) => (data += partData));
+            stream.on("end", () =>
+              fs.appendFile(distPath, data, (err) => {
+                if (err) throw err;
+              })
+            );
+            stream.on("error", (err) => {
+              console.log(`Err:${err}`);
+            });
+          }
+        });
+      }
     });
   });
-  files.forEach((file) => {
-    if (file.isFile()) {
-      const filePath = path.join(__dirname, "styles", file.name);
-      const fileExt = path.parse(filePath).ext;
-      fs.stat(filePath, (err, stats) => {
-        if (err) throw err;
-        if (fileExt === ".css") {
-          const stream = fs.createReadStream(filePath, "utf8");
-          let data = "";
-          stream.on("data", (partData) => (data += partData));
-          stream.on("end", () =>
-            fs.appendFile(distPath, data, (err) => {
-              if (err) throw err;
-            })
-          );
-          stream.on("error", (err) => {
-            console.log(`Err:${err}`);
-          });
-        }
-      });
-    }
-  });
-});
+}
+copyStyle();
